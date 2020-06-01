78bccc77f3421fe2353b87fc856808f8d1d86aa3
Kazuyuki Miyake <k-miyake@zengeeks.io>
2020-06-01 11:31:03 +0900

    add nuxt project

---
+++ pages/posts/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <h1>{{ post.fields.title }}</h1>
+    <div>
+      {{ post.fields.description }}
+    </div>
+  </section>
+</template>
+<script>
+import { createClient } from '~/plugins/contentful.js'
+
+const client = createClient()
+export default {
+  async asyncData({ params, payload }) {
+    const entry = await client.getEntry(params.id)
+    return {
+      post: entry
+    }
+  }
+}
+</script>
