ba375433aa72245e1dbca8407908100adb4bfc77
Kazuyuki Miyake <k-miyake@zengeeks.io>
2021-07-05 06:43:25 +0900

    add ts files

--- pages/index.vue
+++ pages/index.vue
@@ -10,21 +10,20 @@
         </li>
       </ul>
     </div>
-    
   </section>
 </template>
-<script>
-import { createClient } from '~/plugins/contentful.js'
+<script lang="ts">
+import { createClient } from "~/plugins/contentful.js";
 
-const client = createClient()
+const client = createClient();
 export default {
-  async asyncData({ params }) {
+  async asyncData() {
     const entries = await client.getEntries({
-      content_type: 'blogPost'
-    })
+      content_type: "blogPost"
+    });
     return {
       posts: entries.items
-    }
+    };
   }
-}
+};
 </script>
