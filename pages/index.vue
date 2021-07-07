a4fec6515f9d6c738da0142101f9eed86c9be22c
Kazuyuki Miyake <k-miyake@zengeeks.io>
2021-07-07 15:20:45 +0900

    Apply Typescript (#19)
    * add ts files
    
    * add contentful generator
    
    * Update template to TS
    
    * fix MarkdownIt types
    
    * update nuxt.config to TS
    
    * fix tsconfig

--- pages/index.vue
+++ pages/index.vue
@@ -10,21 +10,28 @@
         </li>
       </ul>
     </div>
-    
   </section>
 </template>
-<script>
-import { createClient } from '~/plugins/contentful.js'
+<script lang="ts">
+import Vue from "vue";
+import { Entry } from "contentful";
+import { IBlogPostFields } from "@/contentful/generated/types";
+import { createClient } from "@/plugins/contentful.js";
 
-const client = createClient()
-export default {
-  async asyncData({ params }) {
+const client = createClient();
+export default Vue.extend({
+  async asyncData() {
     const entries = await client.getEntries({
-      content_type: 'blogPost'
-    })
+      content_type: "blogPost"
+    });
     return {
       posts: entries.items
-    }
+    };
+  },
+  data() {
+    return {
+      posts: [] as Entry<IBlogPostFields>[]
+    };
   }
-}
+});
 </script>
